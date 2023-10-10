edeef3f461cd8ae2c1d815e8b1692b99382e2306
Martin lei <17988093+MartinLei@users.noreply.github.com>
2023-10-10 12:36:42 +0200

    Test github action

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -3,6 +3,6 @@ import org.scalatest.funsuite.AnyFunSuite
 class MySuite extends AnyFunSuite:
   test("example test that succeeds") {
     val obtained = 42
-    val expected = 42
+    val expected = 43
     assert(obtained == expected)
   }
