c3ed48bc7e54f13e1ca2fbd450d8868272ace292
Martin lei <17988093+MartinLei@users.noreply.github.com>
2023-10-10 11:55:58 +0200

    Add scalatest

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -1,9 +1,9 @@
-// For more information on writing tests, see
-// https://scalameta.org/munit/docs/getting-started.html
-class MySuite extends munit.FunSuite {
+import org.scalatest.funsuite.AnyFunSuite
+
+class MySuite extends AnyFunSuite {
   test("example test that succeeds") {
     val obtained = 42
     val expected = 42
-    assertEquals(obtained, expected)
+    assert(obtained ==  expected)
   }
 }
